38d0d38e4cfb41f7ca5b14ba13a902133fe64694
Zain Rizvi <ZainRizvi@users.noreply.github.com>
2023-02-08 00:29:56 -0600

    tweak colors

--- calendar_data.py
+++ calendar_data.py
@@ -28,36 +28,85 @@ class Month(NamedTuple):
 class Year(NamedTuple):
     months: List[Month]
 
+color_wheel_ring2 = [
+    "#70c3ed", #light blue
+    "#8ed0b5", #green
+    "#8ec975",
+    "#aed361",
+    "#f6ed60", #yellow
+    "#fdb64e", #orange
+    "#f15b40", #red
+    "#f15f90", #pink
+    "#d671ad",
+    "#9a6db0", #purple
+    "#756bb0", #violet
+    "#6783c2", #dark blue
+]
+
+
+color_wheel_ring3 = [
+    "#f68567", #red
+    "#f489a7", #pink
+    "#df92be",
+    "#ac8bc0", #purple
+    "#9087c0", #violet
+    "#879bce", #dark blue
+    "#96d1f3", #light blue
+    "#abdac6", #green
+    "#abd595",
+    "#c1dd89",
+    "#f9f18c", #yellow
+    "#fec679", #orange
+]
+
+pallet1 = [
+    "#db64d3",
+    "#db6498",
+    "#db6c64",
+    "#dba864",
+    "#d3db64",
+    "#98db64",
+    "#64db6b",
+    "#64dba7",
+    "#64d4db",
+    "#6498db",
+    "#6b64db",
+    "#a864db",
+]
+
+solar_colors = color_wheel_ring2
 solar_year = Year(
     months=[
-        Month("January", [31], "#68C3C4"), #"blue"),
-        Month("February", [29], "#67E168"),
-        Month("March", [31], "#C5F567"),
-        Month("April", [30], "#FFFF68"),
-        Month("May", [31], "pink"),
-        Month("June", [30], "green"),
-        Month("July", [31], "orange"),
-        Month("August", [31], "blue"),
-        Month("September", [30], "pink"),
-        Month("October", [31], "grey"),
-        Month("November", [30], "brown"),
-        Month("December", [31], "grey"),
+        Month("January", [31], solar_colors[0]),
+        Month("February", [29], solar_colors[1]),
+        Month("March", [31], solar_colors[2]),
+        Month("April", [30], solar_colors[3]),
+        Month("May", [31], solar_colors[4]),
+        Month("June", [30], solar_colors[5]),
+        Month("July", [31], solar_colors[6]),
+        Month("August", [31], solar_colors[7]),
+        Month("September", [30], solar_colors[8]),
+        Month("October", [31], solar_colors[9]),
+        Month("November", [30], solar_colors[10]),
+        Month("December", [31], solar_colors[11]),
     ]
 )
 
+
+islamic_colors = color_wheel_ring3
 islamic_year = Year(
     months=[
-        Month("Muharram", [29], "#9BD6D7"),
-        Month("Safar", [29], "#9AEB99"),
-        Month("Rabi al-Awwal", [30], "#DAFA9B"),
-        Month("Rabi at-Thani", [30], "#FFFF99"),
-        Month("Jawad al-Awwal", [29], "pink"),
-        Month("Jawad at-Thani", [30], "green"),
-        Month("Rajab", [30], "orange"),
-        Month("Shabaan", [29], "blue"),
-        Month("Ramadan", [30], "pink"),
-        Month("Shawal", [29], "grey"),
-        Month("Dhu al-Qadah", [30], "brown"),
-        Month("Dhu al-Hijja", [29], "grey"),
+        Month("Muharram", [29], islamic_colors[0]),
+        Month("Safar", [29], islamic_colors[1]),
+        Month("Rabi al-Awwal", [30], islamic_colors[2]),
+        Month("Rabi at-Thani", [30], islamic_colors[3]),
+        Month("Jawad al-Awwal", [29], islamic_colors[4]),
+        Month("Jawad at-Thani", [30], islamic_colors[5]),
+        Month("Rajab", [30], islamic_colors[6]),
+        Month("Shabaan", [29], islamic_colors[7]),
+        Month("Ramadan", [30], islamic_colors[8]),
+        Month("Shawal", [29], islamic_colors[9]),
+        Month("Dhu al-Qadah", [30], islamic_colors[10]),
+        Month("Dhu al-Hijja", [29], islamic_colors[11]),
     ]
 )
--- make_cal.py
+++ make_cal.py
@@ -21,6 +21,7 @@ if is_notebook():
     get_ipython().run_line_magic('autoreload', '2')
 
 import math
+import os
 import svgwrite
 from svgwrite import Drawing
 from IPython.display import SVG
@@ -30,360 +31,91 @@ from typing import List
 
 # custom libs
 from primitives import *
-from arc_drawing import getArc, getDimensionalArc
-from calendar_data import MonthInstance
+from arc_drawing import *
+from calendar_data import *
+from calendar_drawings import *
 
 
 # In[2]:
 
 
+# Get months to draw
+
+canvas_width = 11
+width = inchToMilimeter(8)
+outermost_radius = width / ( 2 * 3.14 / 12)
+# outermost_radius = inchToMilimeter(35)
+inner_radius = outermost_radius * 9.2 / 10
+month_thickness = (outermost_radius - inner_radius)
+date_box_height = month_thickness * 0.18
+
+# width = outermost_radius * 2 * 3.14 / 12 / 2
+width_center = inchToMilimeter(canvas_width) / 2
+vertical_offset = 30
+
+solarMonths = []
+
+for index, month in enumerate(solar_year.months):
+    name_upside_down = (index >= 3 and index < 9)
+    for day in month.num_days:
+        solarMonths.append(
+            MonthInstance(
+                name=month.name,
+                num_days=day,
+                color=month.color,
+                name_upside_down=name_upside_down,
+                date_on_top=False, # the outer month
+                date_box_height=date_box_height,
+                inner_radius=inner_radius,
+                outer_radius=outermost_radius,
+            )
+        ) 
 
 
-
-# In[3]:
-
-
-DATE_FILL_COLOR = "#3A6"
-def getMonth(month: MonthInstance, days_in_year: int, origin: Point):
-    month_width_degrees = 360 * month.num_days / days_in_year
-    # center start and stop angles around -90 degrees
-    angle_offset = month_width_degrees / 2
-    center_angle = -90
-    start_angle = center_angle - angle_offset
-    stop_angle = center_angle + angle_offset
-
-    drawing_elements = []
-    background = getDimensionalArc(
-        origin=origin,
-        inner_radius=month.inner_radius,
-        outer_radius=month.outer_radius,
-        start_angle=start_angle,
-        stop_angle=stop_angle,
-        fill=month.color
-    )
-
-    drawing_elements.append(background)
-    date_width_degrees = month_width_degrees / month.num_days # how many degrees of width the date box should have
-    if month.date_on_top:
-        date_inner_radius = month.outer_radius - month.date_box_height
-        date_outer_radius = month.outer_radius
-        month_text_radius = (date_inner_radius + month.inner_radius) / 2
-    else:
-        date_inner_radius = month.inner_radius
-        date_outer_radius = month.inner_radius + month.date_box_height
-        month_text_radius = (date_outer_radius + month.outer_radius) / 2
-
-    month_text_arc = getArc(
-        origin=origin,
-        radius=month_text_radius,
-        start_angle=start_angle,
-        stop_angle=stop_angle
-    )
-
-    month_text = CurvedText(arc=month_text_arc, text=month.name)
-    # drawing_elements.append(month_text_arc)
-    drawing_elements.append(month_text)
-
-    curr_date_end_angle = start_angle
-    for i in range(month.num_days):
-        curr_date_start_angle = curr_date_end_angle
-        curr_date_end_angle = curr_date_start_angle + date_width_degrees
-
-        date_background = getDimensionalArc(
-            origin=origin,
-            inner_radius=date_inner_radius,
-            outer_radius=date_outer_radius,
-            start_angle=curr_date_start_angle,
-            stop_angle=curr_date_end_angle,
-            fill=DATE_FILL_COLOR
-        )
-
-        drawing_elements.append(date_background)
-
-        
-    
-    return drawing_elements
+islamicMonths = []
+month_offset = 5
+for index, month in enumerate(islamic_year.months):
+    name_upside_down = (index >= 3 and index < 9)
+    for day in month.num_days:
+        islamicMonths.append(
+            MonthInstance(
+                name=month.name,
+                num_days=day,
+                color=month.color,
+                name_upside_down=name_upside_down,
+                date_on_top=True, # the outer month
+                date_box_height=date_box_height,
+                inner_radius=inner_radius - month_thickness - month_offset,
+                outer_radius=outermost_radius - month_thickness - month_offset,
+            )
+        ) 
 
 
 # In[4]:
 
 
-monthInstance = MonthInstance(
-    name="January",
-    num_days=31,
-    date_on_top=False,
-    date_box_height=10,
-    inner_radius=800,
-    outer_radius=900,
-    color = "pink",
-)
+origin_first = Point(width_center, outermost_radius + vertical_offset)
+origin = origin_first
 
-monthInstance2 = MonthInstance(
-    name="January",
-    num_days=31,
-    date_on_top=True,
-    date_box_height=10,
-    inner_radius=700,
-    outer_radius=800,
-    color="blue",
-)
-
-origin = Point(250, 1000)
 days_in_year = 366
 
-def drawMonthParts(dwg, monthParts):
-    for part in monthParts:
-        drawing = part.drawnPath()
-        # some drawn parts return a list of multiple things to draw. Unify both interfaces here. 
-        if not isinstance(drawing, list):
-            drawing = [drawing]
-            
-        for component in drawing:
-            dwg.add(component)
+for i in range(int(len(solarMonths)/2)):
+    dwg = getPageCanvas()
 
-
-# In[5]:
-
-
-
-
-
-# In[12]:
-
-
-dwg = svgwrite.Drawing()
-dwg.viewbox(0,0,500,500)
-
-month1 = getMonth(monthInstance, days_in_year, origin)
-month2 = getMonth(monthInstance2, days_in_year, origin)
-
-
-drawMonthParts(dwg, month1)
-drawMonthParts(dwg, month2)
+    origin = origin_first
+    drawMonthParts(dwg, getMonth(solarMonths[2 * i], days_in_year, origin))
+    drawMonthParts(dwg, getMonth(islamicMonths[2 * i], days_in_year, origin))
     
-dwg.saveas("test_output.svg", pretty=True)
-SVG(dwg.tostring())
+    origin = Point(origin.x, origin.y + (month_thickness * 2.4))
+    drawMonthParts(dwg, getMonth(solarMonths[2 * i + 1], days_in_year, origin))
+    drawMonthParts(dwg, getMonth(islamicMonths[2 * i + 1], days_in_year, origin))
 
+    svg_file = f"out/test_output_{i}.svg"
+    pdf_file = f"out/calendar_page_{i}.pdf"
 
-# In[10]:
-
-
-
-
-
-# In[ ]:
-
-
-# darc = getDimensionalArc(Point(100,100), 60, 90, -135, -45, fill="red")
-# print(darc.path())
-
-# dwg.add(darc.drawnPath(dwg))
-
-# darc = getDimensionalArc(Point(100,120), 60, 70, -135, -45, fill="yellow")
-# dwg.add(darc.drawnPath(dwg))
-
-#dwg.add(dwg.path(d="m 64.64466094067262,64.64466094067262 A 50,50 0 0 1 135.35533905932738,64.64466094067262 L 128.2842712474619,71.7157287525381 A 40,40 71.7157287525381,71.7157287525381L 64.64466094067262,64.64466094067262",
-#stroke="#DDD", fill="none"))
-
-# arc1 = arcPath(Point(100, 100), 50, -135, -45)
-# arc2 = arcPath(Point(100, 120), 50, -45, -135)
-
-# dwg.add(arc1.drawnPath(dwg))
-# dwg.add(arc2.drawnPath(dwg))
-
-
-# In[17]:
-
-
-dwg = svgwrite.Drawing()
-dwg.viewbox(0,0,500,220)
-
-x1 = 200
-y1 = 200
-r1 = 50
-arc = math.sqrt(2*(r1**2))
-
-# setup canvas
-dwg = svgwrite.Drawing()
-dwg.viewbox(0,0,500,220)
-
-# def getTextAlongArc(arc: Arc, text: str):
-  
-
-# Print text centered along path 
-path = dwg.path(
-        d=f"m {x1},{y1} " + # starting point
-          f"a{r1},{r1} 0 0 1 {arc},0 " + # first arc
-          f"a{r1},{r1} 0 0 0 {arc},0 ", # second arc
-        stroke="#DDD",
-        fill="none")
-text = svgwrite.text.Text("")
-text.add(svgwrite.text.TextPath(path, text="soy sol sonatora", startOffset="50%", method="align", text_anchor="middle"))
-dwg.add(path)
-dwg.add(text)
-
-# make_circle(x1,y1,r1)
-# extension = 50
-# (x2, y2) = extend_line_point((x1+r1, y1+r1),(x1,y1), extension)
-# print((x2, y2))
-# extend_circle(x2,y2,50+extension)
+    dwg.saveas(svg_file, pretty=True)
+    # os.system(f"convert {svg_file} {pdf_file}")
+    os.system(f"inkscape {svg_file} --export-pdf={pdf_file}")
 
 SVG(dwg.tostring())
-
-
-# In[ ]:
-
-
-
-
-
-# In[118]:
-
-
-dwg = svgwrite.Drawing()
-dwg.viewbox(0,0,500,220)
-#dwg.add(dwg.line((0, 0), (100, 100), stroke='black'))
-dwg.add(dwg.text('Test', insert=(50, 20)))
-
-for r in range(10,100,5):
-    dwg.add(dwg.circle(center=(300,100), r=r, fill='blue', opacity='0.05'))
-
-#for n in range(11,100,5):
-    #dwg.add(dwg.line((0, 0), (100, n), stroke='black'))
-
-dwg.add(dwg.text("how are you doing", insert=(200, 300)))
-
-x1 = 200
-y1 = 200
-r1 = 50
-arc = math.sqrt(2*(r1**2))
-
-# setup canvas
-dwg = svgwrite.Drawing()
-dwg.viewbox(0,0,500,220)
-
-# Print text centered along path 
-path = dwg.path(
-        d=f"m {x1},{y1} " + # starting point
-          f"a{r1},{r1} 0 0 1 {arc},0 " + # first arc
-          f"a{r1},{r1} 0 0 0 {arc},0 ", # second arc
-        stroke="#DDD",
-        fill="none")
-text = svgwrite.text.Text("")
-text.add(svgwrite.text.TextPath(path, text="soy sol sonatora", startOffset="50%", method="align", text_anchor="middle"))
-dwg.add(path)
-dwg.add(text)
-
-# make_circle(x1,y1,r1)
-# extension = 50
-# (x2, y2) = extend_line_point((x1+r1, y1+r1),(x1,y1), extension)
-# print((x2, y2))
-# extend_circle(x2,y2,50+extension)
-
-SVG(dwg.tostring())
-
-
-# In[ ]:
-
-
-
-
-
-# In[119]:
-
-
-# def extend_line_point(start, ext, len):
-#     m = (ext[0] - start[0])/(ext[1]-start[1])
-#     print (f"(ext[0] - start[0]) = {(ext[0] - start[0])}")
-#     print(f"(ext[1]-start[1]) = {(ext[1]-start[1])}")
-#     print(m)
-#     extension_y = math.sqrt((len**2)/(1+m**2))
-#     extension_x = m * extension_y
-
-#     return (ext[0] - extension_x, ext[1] - extension_y)
-
-
-# def make_circle(x, y, r):
-#     arc = math.sqrt(2*(r**2))
-#     print(f"arc={arc}")
-
-#     dwg.add(dwg.path(
-#             d=f'm {x},{y}  a{r},{r} 0 0 1 {arc},0 ',
-#             stroke="#000",
-#             fill="none"))
-#     dwg.add(dwg.path(
-#             d=f'm {x + arc},{y}  a{r},{r} 0 0 1 0,{arc} ',
-#             stroke="#000",
-#             fill="none"))
-#     dwg.add(dwg.path(
-#             d=f'm {x + arc},{y+arc}  a{r},{r} 0 0 1 {arc * -1}, 0 ',
-#             stroke="#000",
-#             fill="none"))
-#     dwg.add(dwg.path(
-#             d=f'm {x},{y+arc} a{r},{r} 0 0 1 0,{arc * -1} ',
-#             stroke="#000",
-#             fill="none"))
-
-# def extend_circle(x, y, r):
-#     arc = math.sqrt(2*(r**2))
-#     print(f"arc={arc}")
-
-#     dwg.add(dwg.path(
-#             d=f'm {x},{y}  a{r},{r} 0 0 1 {arc},0 ',
-#             stroke="#000",
-#             fill="none"))
-
-
-# In[120]:
-
-
-# setup canvas
-dwg = svgwrite.Drawing()
-dwg.viewbox(0,0,600,250)
-
-# Create a path
-x1 = 20
-y1 = 50
-r1 = 50
-arc = math.sqrt(2*(r1**2))
-path = dwg.path(
-        d=f"m {x1+2*arc},{y1} " + # starting point
-          f"a{r1},{r1} 0 0 1 {-arc},0 " + # first arc
-          f"a{r1},{r1} 0 0 0 {-arc},0 ", # second arc
-        # d=f"m {x1},{y1} " + # starting point
-        #  f"a{r1},{r1} 0 0 1 {arc},0 " + # first arc
-        #  f"a{r1},{r1} 0 0 0 {arc},0 ", # second arc         
-        stroke="#DDD",
-        fill="none")
-
-path2 = dwg.path(
-        d=f"m {x1},{y1} " + # starting point
-          f"a{r1},{r1} 0 0 1 {arc},0 " + # first arc
-          f"a{r1},{r1} 0 0 0 {arc},0 ", # second arc
-        stroke="#DDD",
-        fill="none",
-        transform=f"rotate(180 {x1+arc} {y1})")
-
-# Center text along path 
-text = svgwrite.text.Text("")
-text.add(svgwrite.text.TextPath(path, text="it worked alhamd", startOffset="50%", method="align", text_anchor="middle"))
-# text.add(svgwrite.text.TextPath(path2, text="it worked alhamd", startOffset="50%", method="align", text_anchor="middle"))
-
-# Draw path and text
-dwg.add(path)
-dwg.add(path2)
-dwg.add(text)
-SVG(dwg.tostring())
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
 
